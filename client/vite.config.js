c2c81acc39652f010e1e74146f9c37a681a793d8
Sushindh <sushindh.anandan@gmail.com>
2025-11-30 23:17:56 +0530

    new vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,12 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      '/api': 'https://cliq2025.onrender.com',
-      '/socket.io': {
-        target: 'https://cliq2025.onrender.com',
-        ws: true,
-      },
-    },
+    
   },
 })
